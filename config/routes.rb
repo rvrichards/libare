01ef72b28def6ec304f9f90e13d57659c154a2de
rob <(nothinghere)>
2017-09-07 19:35:54 -0400

    Added content to help and about pages. Added navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => "static_pages#home"
 
-  get 'static_pages/about'
-  get 'static_pages/home'
-  get 'static_pages/help'
-
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
